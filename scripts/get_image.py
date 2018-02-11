072af30934f9ded31cb41ba399cb918fbb5a432c
NicoMitchell <namitc02@louisville.edu>
2018-02-11 10:41:07 -0500

    added options for image source

--- scripts/get_image.py
+++ scripts/get_image.py
@@ -1,9 +1,12 @@
 import subprocess
 
+option = 2
+
 cmd = "sudo rm current.jpg"
 subprocess.call(cmd.split(),cwd="../../darknet/data")
 
-cmd = "wget http://31.16.111.66:8083/record/current.jpg"
+cmd_options = ["wget http://31.16.111.66:8083/record/current.jpg","wget http://80.92.117.241/record/current.jpg", "wget http://83.162.139.253/record/current.jpg"]
+cmd = cmd_options[option]
 print(subprocess.check_output(cmd.split(),cwd="../../darknet/data"))
 
 cmd3 = "./darknet detect cfg/yolo.cfg yolo.weights data/current.jpg -thresh5"
